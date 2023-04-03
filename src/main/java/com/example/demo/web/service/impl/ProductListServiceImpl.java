15218103b96d1669d513cf1df27a6f5f3da0c313
EUNJIN <kejs0226@ejbt.co.kr>
2023-04-03 09:24:20 +0900

    -pom 업데이트 -svg 삭제 -숫자 세자리마다 ',' 추가 -base64 사진 표출기능 추가

--- src/main/java/com/example/demo/web/service/impl/ProductListServiceImpl.java
+++ src/main/java/com/example/demo/web/service/impl/ProductListServiceImpl.java
@@ -2,11 +2,18 @@ package com.example.demo.web.service.impl;
 import com.example.demo.web.dao.ProductListDAO;
 import com.example.demo.web.service.ProductListService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +24,16 @@ public class ProductListServiceImpl implements ProductListService {
     @Override
     public List<Map<String, Object>> getProductList(Map<String, Object> selectValues) throws Exception {
         List<Map<String, Object>> result = productListDAO.getProductList(selectValues);
+
+        try(Stream<Map<String, Object>> resultStream = result.stream()) {
+            resultStream.filter(e -> e != null && e.containsKey("prdt_mainimg")).forEach(e -> {
+                try(InputStream in = Files.newInputStream(Paths.get((e.get("prdt_mainimg")).toString()))) {
+                    e.put("base64", Base64.getEncoder().encodeToString(IOUtils.toByteArray(in)));
+                } catch(IOException e1) {
+                    e1.printStackTrace();
+                }
+            });
+        }
         return result;
     }
 
